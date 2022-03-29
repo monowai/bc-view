d3b00e62486f3ae7c8808bc4a36e7f6990801ec4
Mike Holdsworth <mike@monowai.com>
2022-03-29 14:56:41 +1300

    test: Support docker and localhost. fix: load trns

--- src/pages/api/trns/import.ts
+++ src/pages/api/trns/import.ts
@@ -1,30 +1,34 @@
 import { TransactionUpload } from "@/types/app";
-import { withApiAuthRequired, getAccessToken } from "@auth0/nextjs-auth0";
-import { Kafka } from "kafkajs";
+import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
+import { Kafka, RecordMetadata } from "kafkajs";
 import { getKafkaHosts, getTrnTopic } from "@/core/api/bc-config";
 
 export default withApiAuthRequired(async function writeRows(req, res) {
   await getAccessToken(req, res);
-  writeTrn({ portfolio: req.body.portfolio, row: req.body.row });
+  await writeTrn({ portfolio: req.body.portfolio, row: req.body.row }).catch(console.error);
+  res.status(200).json("ok");
 });
 
 const kafka = new Kafka({
   clientId: "bc-view",
-  brokers: getKafkaHosts,
+  brokers: getKafkaHosts(),
 });
 
-function writeTrn(transactionUpload: TransactionUpload): void {
-  try {
-    const producer = kafka.producer();
-    const messages = [
-      {
-        key: `${transactionUpload.portfolio.id}/${transactionUpload.row[5]}`,
-        value: JSON.stringify(transactionUpload),
-      },
-    ];
-    producer.connect().then();
-    producer.send({ topic: getTrnTopic(), messages: messages }).then();
-  } catch (e: any) {
-    console.error("%s", e.toString());
-  }
+const topic = getTrnTopic();
+
+async function writeTrn(transactionUpload: TransactionUpload): Promise<RecordMetadata[] | void> {
+  console.log(`${getKafkaHosts()} : ${getTrnTopic()}`);
+  const producer = await kafka.producer();
+  await producer.connect();
+  const messages = [
+    {
+      key: `${transactionUpload.portfolio.id}.${transactionUpload.row[5]}`,
+      value: JSON.stringify(transactionUpload),
+    },
+  ];
+  return producer.send({
+    topic,
+    messages: messages,
+    // acks: 1,
+  });
 }
