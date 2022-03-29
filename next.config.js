e52ce46c9751080efa6287f094c4c335d8458620
Mike Holdsworth <mike@monowai.com>
2022-03-29 14:56:41 +1300

    test: Support docker and localhost. fix: load trns

--- next.config.js
+++ next.config.js
@@ -6,18 +6,26 @@ const nextConfig = {
 const path = require("path");
 module.exports = {
   publicRuntimeConfig: {
-    topicTrn: "bc-trn-csv-demo",
+    topicTrn:
+      process.env.NODE_ENV === "development" || process.env.NODE_ENV === "docker"
+        ? "bc-trn-csv-dev" // queue for localhost dev
+        : "bc-trn-csv-demo", // production queue
+
     kafkaUrl:
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:9092" // development api
-        : "http://kafka:9092", // production api
+      process.env.NODE_ENV === "development" || process.env.NODE_ENV === "docker"
+        ? "localhost:9092" // development api
+        : "kafka:9092", // production api
     apiDataUrl:
       process.env.NODE_ENV === "development"
-        ? "http://localhost:9610/api" // development api
+        ? "http://localhost:9510/api" // development api
+        : process.env.NODE_ENV === "docker"
+        ? "http://localhost:9610/api"
         : "http://data:9510/api", // production api
     apiPositionsUrl:
       process.env.NODE_ENV === "development"
-        ? "http://localhost:9600/api" // development api
+        ? "http://localhost:9500/api" // development api
+        : process.env.NODE_ENV === "docker"
+        ? "http://localhost:9510/api"
         : "http://position:9500/api", // production api
   },
   images: {
