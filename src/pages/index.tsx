07ea5c0ddbf64175c7afdd91500311c59892a0e1
Mike Holdsworth <mike@monowai.com>
2022-04-02 09:36:36 +1300

    fix: Homepage auth check

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import { useUser } from "@auth0/nextjs-auth0";
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import Link from "next/link";
 import React from "react";
 import { useTranslation } from "next-i18next";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-export default function Home(): any {
+export default withPageAuthRequired(function Home(): React.ReactElement {
   const { user, error, isLoading } = useUser();
   const { t, ready } = useTranslation("common");
 
@@ -13,6 +13,7 @@ export default function Home(): any {
   if (error) return <div>{error.message}</div>;
 
   if (user) {
+    // noinspection HtmlUnknownTarget
     return (
       <div>
         {t("home.welcome")}
@@ -25,10 +26,11 @@ export default function Home(): any {
       </div>
     );
   }
-
+  // noinspection HtmlUnknownTarget
   return <a href="/api/auth/login">{t("user.login")}</a>;
-}
+});
 
+// noinspection JSUnusedGlobalSymbols
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
   props: {
     ...(await serverSideTranslations(locale as string, ["common"])),
