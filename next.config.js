43789b2246a79552f9e7a4fae9192331e33e8df5
Mike Holdsworth <mike@monowai.com>
2022-04-24 17:32:56 +0800

    chore: project config. Linting

--- next.config.js
+++ next.config.js
@@ -1,42 +1,25 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: true
 };
 const path = require("path");
 module.exports = {
   publicRuntimeConfig: {
-    topicTrn:
-      process.env.NODE_ENV === "development" || process.env.NODE_ENV === "docker"
-        ? "bc-trn-csv-dev" // queue for localhost dev
-        : "bc-trn-csv-demo", // production queue
-
-    kafkaUrl:
-      process.env.NODE_ENV === "development" || process.env.NODE_ENV === "docker"
-        ? "localhost:9092" // development api
-        : "kafka:9092", // production api
-    apiDataUrl:
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:9510/api" // development api
-        : process.env.NODE_ENV === "docker"
-        ? "http://localhost:9610/api"
-        : "http://data:9510/api", // production api
-    apiPositionsUrl:
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:9500/api" // development api
-        : process.env.NODE_ENV === "docker"
-        ? "http://localhost:9600/api"
-        : "http://position:9500/api", // production api
+    topicTrn: process.env.TOPIC_TRN,
+    kafkaUrl: process.env.KAFKA_URL,
+    apiDataUrl: process.env.BC_DATA,
+    apiPositionsUrl: process.env.BC_POSITION
   },
   images: {
-    domains: ["s.gravatar.com", "cdn.auth0.com"],
+    domains: ["s.gravatar.com", "cdn.auth0.com"]
   },
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, "styles")]
   },
   experimental: {
-    outputStandalone: true, // Docker
+    outputStandalone: true // Docker
   },
   i18n,
-  nextConfig,
+  nextConfig
 };
