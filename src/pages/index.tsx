f88c6830803435f0044b0b3c4c8b2eb328daa842
Mike Holdsworth <mike@monowai.com>
2022-12-12 04:45:19 +0100

    refactor: Auth investigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,30 +4,29 @@ import React from "react";
 import { useTranslation } from "next-i18next";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { rootLoader } from "@/core/common/PageLoader";
 
 export default withPageAuthRequired(function Home(): React.ReactElement {
   const { user, error, isLoading } = useUser();
-  const { t, ready } = useTranslation("common");
-
-  if (isLoading || !ready) return <div />;
+  const { t } = useTranslation("common");
+  if (isLoading) return rootLoader(t("loading"));
   if (error) return <div>{error.message}</div>;
-
-  if (user) {
+  if (!user) {
     // noinspection HtmlUnknownTarget
-    return (
-      <div>
-        {t("home.welcome")}
-        <div>
-          <Link href="/portfolios">{t("home.portfolios")}</Link>
-        </div>
-        <div>
-          <Link href="/api/auth/logout">{t("user.logout")}</Link>
-        </div>
-      </div>
-    );
+    return <Link href="/api/auth/login">{t("user.login")}</Link>;
   }
   // noinspection HtmlUnknownTarget
-  return <Link href="/api/auth/login">{t("user.login")}</Link>;
+  return (
+    <div>
+      {t("home.welcome")}
+      <div>
+        <Link href="/portfolios">{t("home.portfolios")}</Link>
+      </div>
+      <div>
+        <Link href="/api/auth/logout">{t("user.logout")}</Link>
+      </div>
+    </div>
+  );
 });
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
