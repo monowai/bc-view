1a6cd1ea3fc5114035b9b95db227213e6423c2b9
Mike Holdsworth <mike@monowai.com>
2023-01-11 09:49:05 +0800

    feat: Rethink column ordering

--- src/domain/holdings/SubTotal.tsx
+++ src/domain/holdings/SubTotal.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { GroupedSubtotals } from "../../core/types/beancounter";
-import { FormatValue } from "../../core/common/MoneyUtils";
+import { GroupedSubtotals } from "@/types/beancounter";
+import { FormatValue } from "@/core/common/MoneyUtils";
 
 export function SubTotal({ groupBy, subTotals, valueIn }: GroupedSubtotals): JSX.Element {
   return (
@@ -13,14 +13,13 @@ export function SubTotal({ groupBy, subTotals, valueIn }: GroupedSubtotals): JSX
           <FormatValue value={subTotals[valueIn].gainOnDay} />
         </td>
         <td colSpan={2} align={"right"}>
+          <FormatValue value={subTotals[valueIn].costValue} />
+        </td>
+        <td colSpan={2} align={"right"}>
           <FormatValue value={subTotals[valueIn].marketValue} />
         </td>
-        <td align={"right"}>
+        <td colSpan={1} align={"right"}>
           <FormatValue value={subTotals[valueIn].unrealisedGain} />
-        </td>
-        <td />
-        <td align={"right"}>
-          <FormatValue value={subTotals[valueIn].costValue} />
         </td>
         <td />
         <td align={"right"}>
