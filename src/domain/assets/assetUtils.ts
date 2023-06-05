4227d4a8161ed4c5655fa9b843351899ed3ee88e
Mike Holdsworth <mike@monowai.com>
2023-06-05 12:02:18 +0800

    feat: Asset Display Name

--- src/domain/assets/assetUtils.ts
+++ src/domain/assets/assetUtils.ts
@@ -7,9 +7,14 @@ export function isCash(asset: Asset): boolean {
   return asset.assetCategory.id === "CASH";
 }
 
-export function assetName(asset: Asset): string {
+export function displayName(asset: Asset): string {
   if (isCash(asset)) {
     return asset.name;
+  }
+  if (asset.code.indexOf(".") > 0) {
+    return `${asset.code.substring(asset.code.indexOf(".") + 1)}: ${
+      asset.name
+    }`;
   }
   return `${asset.code}: ${asset.name}`;
 }
