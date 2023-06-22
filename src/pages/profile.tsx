f41460fca5a83d5033252f11bfc66283db3c68d8
Mike Holdsworth <mike@monowai.com>
2023-06-22 08:47:25 +0800

    feat: Enhance profile handling

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,9 +1,27 @@
-import { useUser } from "@auth0/nextjs-auth0/client";
+import { UserProfile, useUser } from "@auth0/nextjs-auth0/client";
 import Image from "next/image";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { rootLoader } from "@core/common/PageLoader";
 import { useTranslation } from "next-i18next";
+
+export function getAvatar(user: UserProfile, size: number) {
+  return (
+    <Image
+      src={user.picture as string}
+      alt={user.name as string}
+      style={{
+        borderRadius: "50%",
+        width: size,
+        height: size,
+        display: "block",
+      }}
+      priority={true}
+      width={size}
+      height={size}
+    />
+  );
+}
 
 export default function Profile(): JSX.Element {
   const { user, error, isLoading } = useUser();
@@ -13,15 +31,11 @@ export default function Profile(): JSX.Element {
   if (!user) return <div>t("user.notfound")</div>;
 
   return (
-    <div>
-      <Image
-        src={user.picture as string}
-        alt={user.name as string}
-        width={500}
-        height={500}
-      />
-      <h2>{user.name}</h2>
-      <p>{user.email}</p>
+    <div className={"box"}>
+      <h1 className="subtitle">
+        {user.nickname} / {user.email}
+      </h1>
+      {getAvatar(user, 50)}
     </div>
   );
 }
