6a904095c1e41a6420c373fe14a47bce5503d563
Mike Holdsworth <mike@monowai.com>
2023-06-29 19:33:34 +0800

    feat: Portfolio validation

--- src/core/components/currency.ts
+++ src/core/components/currency.ts
@@ -1,19 +1,16 @@
-import React, { ReactElement } from "react";
 import { Currency, CurrencyOption } from "@core/types/beancounter";
-import Select from "react-select";
 
-export const usd: CurrencyOption = { value: "USD", label: "USD" }
+// export const usd: CurrencyOption = { value: "USD", label: "USD" };
 export function currencyOptions(currencies: Currency[]): CurrencyOption[] {
   return currencies.map((currency) => {
     return { value: currency.code, label: currency.code };
   });
 }
-function toCurrencyOption(currency: Currency): CurrencyOption {
+export function toCurrencyOption(currency: Currency): CurrencyOption {
   return { value: currency.code, label: currency.code };
 }
 
 export function toCurrency(id: string, currencies: Currency[]): CurrencyOption {
   const currency = currencies.filter((option) => option.code === id);
-  return toCurrencyOption(currency[0])
+  return toCurrencyOption(currency[0]);
 }
-
