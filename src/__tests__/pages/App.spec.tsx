e91865ce8928e21bb912f10be65bb72649799df5
Mike Holdsworth <mike@monowai.com>
2023-06-30 13:09:15 +0800

    fix: Regression

--- src/__tests__/pages/App.spec.tsx
+++ src/__tests__/pages/App.spec.tsx
@@ -2,10 +2,12 @@ import Home from "@pages/index";
 import React from "react";
 import { screen, render } from "@testing-library/react";
 import { mockUser, withUserProvider } from "../fixtures";
-
+import fetchMock, {enableFetchMocks} from "jest-fetch-mock";
+enableFetchMocks();
 jest.mock("react-i18next", () => ({
   useTranslation: () => ({ ready: true, t: (key: string) => key }),
 }));
+
 
 afterEach(() => {
   jest.clearAllMocks();
@@ -13,11 +15,23 @@ afterEach(() => {
   jest.resetModules();
 });
 
-describe("<App />", () => {
-  test("renders for authorised user", () => {
+describe("<Home />", () => {
+  test("renders for authorised user", async () => {
+    fetchMock.mockResponseOnce(
+      JSON.stringify({
+        data:
+          {
+            id: "ownerId",
+            email: "mike@bc.com",
+            active: true,
+            since: "2020-03-03",
+          },
+      })
+    );
     render(<Home user={mockUser} />, {
       wrapper: withUserProvider({ user: mockUser }),
     });
+    expect(await screen.findByTestId("loading")).toBeInTheDocument();
     const heading = screen.getByText("home.welcome");
     expect(heading).toBeInTheDocument();
     expect(screen.getByText("home.portfolios")).toBeInTheDocument();
