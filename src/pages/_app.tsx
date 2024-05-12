c34a2187ba9916f34aafc7cc3afb214ec87370f9
Mike Holdsworth <mike@monowai.com>
2024-05-12 16:23:05 +0800

    chore: Library upgrades

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
-import React, { ReactElement } from "react";
-import "@styles/bc.sass";
+import React from "react";
 import type { AppProps } from "next/app";
 import { appWithTranslation } from "next-i18next";
 import Header from "@pages/header/Header";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import "@styles/bc.sass";
 
-function App({ Component, pageProps }: AppProps): ReactElement {
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <UserProvider>
-      <div className={"page.box"}>
+      <div className="page.box">
         <Header />
         <Component {...pageProps} />
       </div>
