1706ff8b67b8cb6877f767b30f385823786e92ee
Mike Holdsworth <mike@monowai.com>
2025-01-08 11:31:45 +0800

    Revert "chore: Library upgrades"
    This reverts commit 1031a5275d843673bbe3387a9bceb71ace3b6139.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,11 @@ import React from "react"
 import type { AppProps } from "next/app"
 import { appWithTranslation } from "next-i18next"
 import Header from "@pages/header/Header"
-import Modal from "react-modal"
 import { UserProvider } from "@auth0/nextjs-auth0/client"
 import "@styles/bc.sass"
 import GitInfo from "@components/GitInfo"
 import { useRouter } from "next/router"
 
-Modal.setAppElement("#root")
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   const router = useRouter()
   const alwaysVisible =
