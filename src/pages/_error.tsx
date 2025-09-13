4409e68b8c9c8e277324c1a3f06513743f47b19b
Mike Holdsworth <mike@monowai.com>
2025-09-13 10:18:25 +0800

    chore: OpenTelemetry/Sentry Turbopack config

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -9,7 +9,7 @@ interface CustomErrorComponentProps {
 
 const CustomErrorComponent = ({
   statusCode,
-}: CustomErrorComponentProps): JSX.Element => {
+}: CustomErrorComponentProps): React.ReactElement => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-6 rounded-lg shadow-lg text-center">
