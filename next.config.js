f73b97f95c0296164fdf92fd82dad229c4809b23
Mike Holdsworth <mike@monowai.com>
2025-12-24 06:29:15 +0800

    devops: Sentry, OpenTelemetry and Turbopack

--- next.config.js
+++ next.config.js
@@ -110,50 +110,40 @@ const { withSentryConfig } = require("@sentry/nextjs")
 
 module.exports = withSentryConfig(module.exports, {
   // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
   org: "monowai-developments-ltd",
   project: "bc-view",
+
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-  // Disable Sentry plugins if no auth token is provided
-  disableServerWebpackPlugin:
-    !process.env.SENTRY_AUTH_TOKEN ||
-    process.env.SENTRY_AUTH_TOKEN === "undefined",
-  disableClientWebpackPlugin:
-    !process.env.SENTRY_AUTH_TOKEN ||
-    process.env.SENTRY_AUTH_TOKEN === "undefined",
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Automatically annotate React components to show their full name in breadcrumbs and session replay
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-
+  // Source maps configuration
   sourcemaps: {
+    // Disable source map upload if no auth token
+    disable:
+      !process.env.SENTRY_AUTH_TOKEN ||
+      process.env.SENTRY_AUTH_TOKEN === "undefined",
     deleteSourcemapsAfterUpload: true,
   },
 
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
+  // Upload a larger set of source maps for prettier stack traces
+  widenClientFileUpload: true,
+
+  // Route browser requests through Next.js to circumvent ad-blockers
   tunnelRoute: "/monitoring",
 
-  // Hides source maps from generated client bundles
-  hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
+  // Webpack-specific options
+  webpack: {
+    // Automatically annotate React components for breadcrumbs and replay
+    reactComponentAnnotation: {
+      enabled: true,
+    },
+    // Tree-shake Sentry logger statements
+    treeshake: {
+      removeDebugLogging: true,
+    },
+    // Automatic instrumentation of Vercel Cron Monitors
+    automaticVercelMonitors: true,
+  },
 })
