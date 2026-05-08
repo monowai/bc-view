d7a9272cd5c9899afcdb66ea24a50374492e9b84
Mike Holdsworth <mike@monowai.com>
2026-05-08 13:32:52 +0700

    feat(bc-view): index benchmark overlay on PerformanceChart (#657)

---
+++ src/pages/api/assets/market/[code].ts
@@ -0,0 +1,12 @@
+import {
+  createApiHandler,
+  sanitizePathParam,
+} from "@utils/api/createApiHandler"
+import { getDataUrl } from "@utils/api/bcConfig"
+
+export default createApiHandler({
+  url: (req) => {
+    const code = sanitizePathParam(req.query.code, "code")
+    return getDataUrl(`/assets/market/${code}`)
+  },
+})
